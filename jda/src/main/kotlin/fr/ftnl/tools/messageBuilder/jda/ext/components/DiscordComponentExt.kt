ef6ce70fe8e1d69de763c6ae0fac6e7923adecb8
ocelus <contact@ftnl.fr>
2026-02-13 15:24:14 +0100

    feat: add conversion functions for MessageTopLevelComponent and ModalTopLevelComponent
    Introduce new functions to convert compatible components to JDA message and modal components, while deprecating the old conversion methods.

--- jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/DiscordComponentExt.kt
+++ jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/DiscordComponentExt.kt
@@ -18,9 +18,13 @@ import fr.ftnl.tools.messageBuilder.jda.ext.components.layout.section.toJda
 import fr.ftnl.tools.messageBuilder.jda.ext.components.layout.separator.toJda
 
 import net.dv8tion.jda.api.components.Component
+import net.dv8tion.jda.api.components.MessageTopLevelComponent
+import net.dv8tion.jda.api.components.ModalTopLevelComponent
 
-
+@Deprecated("use toJdaMessageComponents or toJdaModalComponents instead")
 fun <T: Component> List<DiscordComponent>.toJdaComponents(): List<T> = mapNotNull { it.toJdaComponents() as? T }
+
+@Deprecated("use toJdaMessageComponents or toJdaModalComponents instead")
 fun DiscordComponent.toJdaComponents(): Component {
     return when (this) {
         // --- Interactive Components ---
@@ -43,4 +47,46 @@ fun DiscordComponent.toJdaComponents(): Component {
         
         else -> throw IllegalArgumentException("Conversion JDA non supportée pour le type: ${this::class.simpleName}")
     }
+}
+
+
+
+fun <T: MessageTopLevelComponent> List<MessageCompatibleComponent>.toJdaMessageComponents(): List<T> = mapNotNull { it.toJdaMessageComponents() as? T }
+fun MessageCompatibleComponent.toJdaMessageComponents(): MessageTopLevelComponent {
+    return convertToJdaComponent() as MessageTopLevelComponent
+}
+
+fun <T: ModalTopLevelComponent> List<ModalCompatibleComponent>.toJdaModalComponents(): List<T> = mapNotNull { it.toJdaModalComponents() as? T }
+fun ModalCompatibleComponent.toJdaModalComponents(): ModalTopLevelComponent {
+    return convertToJdaComponent() as ModalTopLevelComponent
+}
+
+private fun DiscordComponent.convertToJdaComponent() = when (this) {
+    // --- Content Components ---
+    is TextDisplay          -> this.toJda()
+    is MediaGallery         -> this.toJda()
+    // FileComponent
+    // Thumbnail component
+    
+    // --- Interactive Components ---
+    is Button               -> this.toJda()
+    // checkbox
+    // checkboxGroup
+    is UserSelect           -> this.toJda()
+    is RoleSelect           -> this.toJda()
+    is ChannelSelect        -> this.toJda()
+    is MentionableSelect    -> this.toJda()
+    // fileUpload
+    // radioGroup
+    is StringSelect         -> this.toJda()
+    is TextInput            -> this.toJda()
+    
+    // --- Layout Components ---
+    is ActionRow            -> this.toJda()
+    is Container            -> this.toJda()
+    // Label
+    is Section              -> this.toJda()
+    is Separator            -> this.toJda()
+    
+    else -> throw IllegalArgumentException("Conversion JDA non supportée pour le type: ${this::class.simpleName}")
 }
