7cfe6d4c6bf64aaf2c144bed8f277e30bf3e8480
ocelus <contact@ftnl.fr>
2026-02-13 22:29:15 +0100

    refactor: update createSection and createLabel methods to use appropriate component types
    Modified createSection to no longer require an accessory component and updated createLabel to accept LabelChildComponent instead of DiscordComponent. Additionally, changed createButton to use ButtonStyles for improved type safety.

--- jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/layout/container/ContainerExt.kt
+++ jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/layout/container/ContainerExt.kt
@@ -1,13 +1,14 @@
 package fr.ftnl.tools.messageBuilder.jda.ext.components.layout.container
 
 import fr.ftnl.tools.messageBuilder.core.dto.components.layout.Container
+import fr.ftnl.tools.messageBuilder.jda.ext.components.convertToJdaComponent
 import fr.ftnl.tools.messageBuilder.jda.ext.components.toJdaComponents
 import net.dv8tion.jda.api.components.container.ContainerChildComponent as JdaContainerChild
 import net.dv8tion.jda.api.components.container.Container as JdaContainer
 
 
 fun Container.toJda(): JdaContainer {
-    val builder = JdaContainer.of(this.components.mapNotNull { it.toJdaComponents() as? JdaContainerChild })
+    val builder = JdaContainer.of(this.components.elements.mapNotNull { it.convertToJdaComponent() as? JdaContainerChild })
     
     if (accentColor != null) builder.withAccentColor(accentColor)
     builder.withSpoiler(spoiler)
--- jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/layout/section/SectionExt.kt
+++ jda/src/main/kotlin/fr/ftnl/tools/messageBuilder/jda/ext/components/layout/section/SectionExt.kt
@@ -1,13 +1,16 @@
 package fr.ftnl.tools.messageBuilder.jda.ext.components.layout.section
 
 import fr.ftnl.tools.messageBuilder.core.dto.components.layout.Section
+import fr.ftnl.tools.messageBuilder.jda.ext.components.convertToJdaComponent
 import fr.ftnl.tools.messageBuilder.jda.ext.components.toJdaComponents
 import net.dv8tion.jda.api.components.section.SectionAccessoryComponent
 import net.dv8tion.jda.api.components.section.SectionContentComponent
 import net.dv8tion.jda.api.components.section.Section as JdaSection
 
 fun Section.toJda(): JdaSection {
-    val accessory = accessory.toJdaComponents() as SectionAccessoryComponent
-    val components = components.map { it.toJdaComponents() as SectionContentComponent }
+    requireNotNull(accessory) { "Section must have an accessory" }
+    require(components.elements.isNotEmpty()) { "Section must have at least one component" }
+    val accessory = accessory!!.convertToJdaComponent() as SectionAccessoryComponent
+    val components = components.elements.map { it.convertToJdaComponent() as SectionContentComponent }
     return JdaSection.of(accessory, components)
 }
